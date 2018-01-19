ec14bef7573ce63022db479b23bcdbb46e8d9af6
futurist <jamesyang999@gmail.com>
2018-01-19 08:32:11 +0800

    add noMergeCRLF option

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
 const LF = '\n'
 const CR = '\r'
 
-function locationToIndex (src, line, column) {
+function locationToIndex (src, line, column, noMergeCRLF) {
   let index = 0  // total index
   let count = 1  // line count
   for (let c, i = 0; count < line && i < src.length; i++) {
     index++
     c = src[i]  // cur char
-    if (c == LF || c == CR) {
+    if (c === LF || c === CR) {
       count++
-      if (c == CR && src[i + 1] == LF) {
+      if (c === CR && src[i + 1] === LF) {
         // it's dos newline \r\n
         i++
+        noMergeCRLF && index++
       }
     }
   }
-  return index + (column || 0)
+  return index + (column|0)
 }
 
 
-function indexToLocation (src, index) {
+function indexToLocation (src, index, noMergeCRLF) {
   let line = 1  // total idx
   let count = 0  // line count
   let startIndex = 0
   for (let c, i = 0; count < index && i < src.length; i++) {
     count++
     c = src[i]  // cur char
-    if (c == LF || c == CR) {
+    if (c === LF || c === CR) {
       line++
       startIndex = count
-      if (c == CR && src[i + 1] == LF) {
+      if (c === CR && src[i + 1] === LF) {
         // it's dos newline \r\n
         i++
+        noMergeCRLF && count++
       }
     }
   }
--- test/test.js
+++ test/test.js
@@ -14,6 +14,10 @@ describe('locationToIndex', function() {
     var src = 'var a=1;\r\n\r\n3\n4\r5var b=2;'
     expect(lib.locationToIndex(src, 5, 2)).equal(16)
   })
+  it('noMergeCRLF: multiple consecutive \r\n', function() {
+    var src = 'var a=1;\r\n\r\n3\n4\r5var b=2;'
+    expect(lib.locationToIndex(src, 5, 2, true)).equal(18)
+  })
 })
 
 describe('indexToLocation', function() {
@@ -24,5 +28,9 @@ describe('indexToLocation', function() {
   it('out of range', function() {
     var src = 'var a=1;\r\n2\n3\r4var b=2;'
     expect(lib.indexToLocation(src, 115)).deep.equal({line: 4, column: 9})
+  })
+  it('noMergeCRLF: within range', function() {
+    var src = 'var a=1;\r\n2\n3\r4var b=2;'
+    expect(lib.indexToLocation(src, 15, true)).deep.equal({line: 4, column: 1})
   })
 })
